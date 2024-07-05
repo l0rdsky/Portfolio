b1551b6f8b144a91cf96fec207883c90145e1674
Akash pawar <ap28198@gmail.com>
2024-07-05 16:04:34 +0530

    corrected magic button bug

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -18,7 +18,7 @@ const MagicButton = ({
   return (
     <button
       className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-lg p-[1px] focus:outline-none"
-      onClick={handleSubmit} // Call handleSubmit when button is clicked
+      onClick={handleClick} // Call handleSubmit when button is clicked
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
